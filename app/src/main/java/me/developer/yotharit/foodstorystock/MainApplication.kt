0042a1c43c6a02585c146ea711156d3dd8cc7fbc
Tharit Pongsaneh <yo.tharit@hotmail.com>
2018-06-07 12:32:12 +0700

    first commit

---
+++ app/src/main/java/me/developer/yotharit/foodstorystock/MainApplication.kt
@@ -0,0 +1,14 @@
+package me.developer.yotharit.foodstorystock
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
